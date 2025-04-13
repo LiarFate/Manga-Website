a2f66d7a5d6d730adddeb1d2d18c20754915c3bc
✰Liar✰ <107493523+LiarFate@users.noreply.github.com>
2025-04-13 07:26:52 +0700

    Add files via upload

---
+++ mangaDataChinese.js
@@ -0,0 +1,255 @@
+const mangaListChinese = [
+    //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+    
+        
+                                                            // ██████╗██╗  ██╗██╗███╗   ██╗███████╗███████╗███████╗
+                                                            //██╔════╝██║  ██║██║████╗  ██║██╔════╝██╔════╝██╔════╝
+                                                            //██║     ███████║██║██╔██╗ ██║█████╗  ███████╗█████╗  
+                                                            //██║     ██╔══██║██║██║╚██╗██║██╔══╝  ╚════██║██╔══╝  
+                                                            //╚██████╗██║  ██║██║██║ ╚████║███████╗███████║███████╗
+                                                            // ╚═════╝╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝╚══════╝╚══════╝╚══════╝
+                                                         
+    
+    //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//                                           
+          {
+            href: "https://www.facebook.com/KrondGrean",
+            title: "เคล็ดวิชา โคตรเกรียน",
+            image: "https://i.imgur.com/64gcqgq.png"
+          },
+          {
+            href: "https://www.facebook.com/Paamua",
+            title: "เคล็ดวิชา แปลมั่ว",
+            image: "https://i.imgur.com/GeYYz1V.png"
+          },
+          {
+            href: "https://www.facebook.com/KredWichaSinRok",
+            title: "เคล็ดวิชา วันสิ้นโลก",
+            image: "https://i.imgur.com/Z30EQCf.png"
+          },
+          {
+            href: "https://www.facebook.com/HaremWicha",
+            title: "เคล็ดวิชา ฮาเร็ม",
+            image: "https://i.imgur.com/vypotvi.png"
+          },
+          {
+            href: "https://www.facebook.com/Tumlailang",
+            title: "เคล็ดวิชา ทำลายล้าง",
+            image: "https://i.imgur.com/rcTqhFp.png"
+          },
+          {
+            href: "https://www.facebook.com/PaaRyaeRyae",
+            title: "เคล็ดวิชา แปลไปเรื่อยๆ",
+            image: "https://imgur.com/hWRfNJg.png"
+          },
+          {
+            href: "https://www.facebook.com/MangaFantaa",
+            title: "Fanta มังงะแปล",
+            image: "https://imgur.com/XYJVi6o.png"
+          },
+          {
+            href: "https://www.facebook.com/Fantamaokaow",
+            title: "Fanta มังงะเมากาว",
+            image: "https://imgur.com/rRI526l.png"
+          },
+          {
+            href: "https://www.facebook.com/DarkrabbitTranslate",
+            title: "กระต่ายทมิฬนักแปล",
+            image: "https://i.imgur.com/RKeTnmJ.png"
+          },
+          {
+            href: "https://www.facebook.com/si2trans",
+            title: "Si2trans",
+            image: "https://imgur.com/kQHuWZC.png"
+          },
+          {
+            href: "https://www.facebook.com/profile.php?id=61574644450948",
+            title: "KongkangTranslate",
+            image: "https://imgur.com/Tk3WZ9o.png"
+          },
+          {
+            href: "https://www.facebook.com/WELoveMelonWarn",
+            title: "LUCRE Lovers-Club",
+            image: "https://imgur.com/joo6org.png"
+          },
+          {
+            href: "https://www.facebook.com/tranharem",
+            title: "เปิดฮาเร็มแปลมั่ว",
+            image: "https://i.imgur.com/MfgufWp.png"
+          },
+          {
+            href: "https://www.facebook.com/mahalnwharem",
+            title: "เปิดฮาเร็มมหาเทพ",
+            image: "https://imgur.com/FyzSu0F.png"
+          },
+          {
+            href: "https://www.facebook.com/kengnishido",
+            title: "เก่งในชิโด",
+            image: "https://imgur.com/fbqyqZa.png"
+          },
+          {
+            href: "https://www.facebook.com/MangaChungKILO",
+            title: "แครอท ชั่งกิโล",
+            image: "https://i.imgur.com/bsFCtpP.png"
+          },
+          {
+            href: "https://www.facebook.com/MangaCarrot",
+            title: "แครอท แปลมังงะ",
+            image: "https://i.imgur.com/nIdWB8f.png"
+          },
+          {
+            href: "https://www.facebook.com/CarrotDaggManga",
+            title: "แครอท แทะมังงะ",
+            image: "https://i.imgur.com/l1R3cjB.png"
+          },
+          {
+            href: "https://www.facebook.com/SiyeTL",
+            title: "ซื่อเย่",
+            image: "https://i.imgur.com/tV3XJrh.png"
+          },
+          {
+            href: "https://www.facebook.com/indytranslate",
+            title: "แปลไทยตามใจฉัน",
+            image: "https://i.imgur.com/4j5VKv8.png"
+          },
+          {
+            href: "https://www.facebook.com/profile.php?id=100063480679248",
+            title: "เเปลเล่นๆ",
+            image: "https://i.imgur.com/rgDmnin.png"
+          },
+          {
+            href: "https://www.facebook.com/NNNuuuu",
+            title: "ขอหัดแปลก่อนน",
+            image: "https://i.imgur.com/vJQ3wqX.png"
+          },
+          {
+            href: "https://www.facebook.com/p/Botunkung-100063861112515",
+            title: "Botunkung",
+            image: "https://i.imgur.com/lKv9iQx.png"
+          },
+          {
+            href: "https://www.facebook.com/p/%C9%AA%C9%B4%E1%B4%9C%E1%B4%8D%E1%B4%80%E1%B4%8B%C9%AA-%D2%93s-100076092690973",
+            title: "เซียนน้อยจุติมาแปล",
+            image: "https://i.imgur.com/9ULIU4K.png"
+          },
+          {
+            href: "https://www.facebook.com/Nongmaxbogbog",
+            title: "น้องแมค แปลมังงะ",
+            image: "https://i.imgur.com/Dch3eyp.png"
+          },
+          {
+            href: "https://www.facebook.com/p/PonyGang-61555860553087",
+            title: "Ponygang",
+            image: "https://imgur.com/Z8ZRMNj.png"
+          },
+          {
+            href: "https://www.facebook.com/MangaImtheone",
+            title: "แปลมังงะ by Imtheone",
+            image: "https://i.imgur.com/8CwkV0L.png"
+          },
+          {
+            href: "https://www.facebook.com/EimiTranslate",
+            title: "ขยันแปล ตามอารมณ์",
+            image: "https://i.imgur.com/c9e2WN1.png"
+          },
+          {
+            href: "https://www.facebook.com/Supitchaya2546",
+            title: "แปลเรื่องเดียว",
+            image: "https://i.imgur.com/GLk27aQ.png"
+          },
+          {
+            href: "https://www.facebook.com/ANNPAIPLAPAI",
+            title: "อ่านไปแปลไป",
+            image: "https://i.imgur.com/pYafKbk.png"
+          },
+          {
+            href: "https://www.facebook.com/Plapaiannpai",
+            title: "แปลไปอ่านไป",
+            image: "https://i.imgur.com/viFkk51.png"
+          },
+          {
+            href: "https://www.facebook.com/profile.php?id=100091939401680",
+            title: "Sect Q แปลมังงะ",
+            image: "https://i.imgur.com/O5TMO4F.png"
+          },
+          {
+            href: "https://www.facebook.com/21402s",
+            title: "Diary'Me",
+            image: "https://i.imgur.com/Tb3HcDh.png"
+          },
+          {
+            href: "https://www.facebook.com/%E0%B9%81%E0%B8%9B%E0%B8%A5%E0%B9%84%E0%B8%94%E0%B9%89%E0%B8%A3%E0%B9%89%E0%B8%B2%E0%B8%A2%E0%B8%81%E0%B8%B2%E0%B8%88%E0%B8%99%E0%B8%B1%E0%B8%81%E0%B8%99%E0%B8%B0-100063561254303",
+            title: "แปลได้ร้ายกาจนักนะ",
+            image: "https://imgur.com/xzpAlLF.png"
+          },
+          {
+            href: "https://www.facebook.com/NoTimr.King",
+            title: "ไม่ค่อยว่างแปล",
+            image: "https://imgur.com/d2en4pa.png"
+          },
+          {
+            href: "https://www.niceoppai.net/Gao-Wu-Landing-in-the-next-10000-years",
+            title: "Gao Wu Landing in the next 10000 years",
+            image: "https://imgur.com/KsWpzSH.png"
+          },
+          {
+            href: "https://www.tanuki-manga.com/manga/rebirth-of-the-urban-immortal-cultivator",
+            title: "Rebirth Of The Urban Immortal Cultivator",
+            image: "https://imgur.com/boLqcmu.png"
+          },
+          {
+            href: "https://www.tanuki-manga.com/manga/chaotic-sword-god-remake",
+            title: "Chaotic Sword God (Remake)",
+            image: "https://imgur.com/mTgLJl5.png"
+          },
+          {
+            href: "https://www.tanuki-manga.com/manga/absolute-resonance",
+            title: "Absolute Resonance",
+            image: "https://imgur.com/Gj2mnmh.png"
+          },
+          {
+            href: "https://god-manga.com/series/magic-emperor",
+            title: "Magic Emperor",
+            image: "https://i.imgur.com/O1xL8ZG.png"
+          },
+          {
+            href: "https://www.xn--l3c0azab5a2gta.com/manga/bailian-chengshen",
+            title: "Bailian Chengshen",
+            image: "https://i.imgur.com/v5VdnSQ.png"
+          },
+          {
+            href: "https://www.tanuki-manga.com/manga/martial-peak",
+            title: "Martial Peak",
+            image: "https://i.imgur.com/P4S3MLH.png"
+          },
+          {
+            href: "https://manga-lc.net/manga/my-house-of-horrors",
+            title: "My House Of Horrors",
+            image: "https://i.imgur.com/06TQ1f3.png"
+          },
+          {
+            href: "https://www.108-manga.com/manga/your-talent-is-mine",
+            title: "Your Talent is Mine",
+            image: "https://i.imgur.com/yTeQrBw.png"
+          },
+          {
+            href: "https://readrealm.co/comic/pRbKaMELO72xglzP41qW",
+            title: "อาจารย์ ศิษย์บ้าขอกบฎนะขอรับ",
+            image: "https://imgur.com/YCsSNZQ.png"
+          },
+          {
+            href: "https://readrealm.co/comic/NazlwDvqOAvVbkJoedBW",
+            title: "มาถึงก็ขายโอสถเซียนโบราณ แม้แต่จอมเทพยังหวาดผวา",
+            image: "https://imgur.com/0K3ACWI.png"
+          },
+          {
+            href: "https://readrealm.co/comic/0NRm4EX9VEQZV63qZJ2W",
+            title: "The Heavenly Path Is Not Stupid วิถีสวรรค์พันธุ์เทพ",
+            image: "https://imgur.com/MsI89n2.png"
+          },
+          {
+            href: "https://readrealm.co/comic/jMvyBR09xzdQVbd1NrqE",
+            title: "ซวยแล้วข้าโดนตามล่าจากลูกศิษย์",
+            image: "https://imgur.com/kcq4E0M.png"
+          }
+
+        ];
---
+++ mangaDataKorean.js
@@ -0,0 +1,225 @@
+const mangaListKorean = [    
+    //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+    
+          
+                                                            //██╗  ██╗ ██████╗ ██████╗ ███████╗ █████╗ ███╗   ██╗//
+                                                            //██║ ██╔╝██╔═══██╗██╔══██╗██╔════╝██╔══██╗████╗  ██║//
+                                                            //█████╔╝ ██║   ██║██████╔╝█████╗  ███████║██╔██╗ ██║//
+                                                            //██╔═██╗ ██║   ██║██╔══██╗██╔══╝  ██╔══██║██║╚██╗██║//
+                                                            //██║  ██╗╚██████╔╝██║  ██║███████╗██║  ██║██║ ╚████║//
+                                                            //╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═══╝//    
+    
+    
+    //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//                        
+    {
+        href: "https://www.facebook.com/Midnight.LvMAX",
+        title: "บุรุษเที่ยงคืน Lv.Max",
+        image: "https://i.imgur.com/hCjhLE1.png"
+      },
+      {
+        href: "https://www.facebook.com/profile.php?id=61552392311040",
+        title: "นักล่ากลับชาติมาแปล",
+        image: "https://i.imgur.com/J1NoMOx.png"
+      },
+      {
+        href: "https://www.facebook.com/LamiresTrans",
+        title: "Lamires Translator",
+        image: "https://i.imgur.com/tD9KQpE.png"
+      },
+      {
+        href: "https://www.facebook.com/Night.Trans",
+        title: "นักแปลยามราตรี",
+        image: "https://imgur.com/WMogEYG.png"
+      },
+      {
+        href: "https://www.facebook.com/@Shadowwtrans",
+        title: "Shadoww",
+        image: "https://imgur.com/A49aMmi.png"
+      },
+      {
+        href: "https://www.nabee-manga.com/manga/the-novels-extra-remake",
+        title: "The Novel’s Extra (Remake)",
+        image: "https://imgur.com/enPs06l.png"
+      },
+      {
+        href: "https://www.xn--l3c0azab5a2gta.com/manga/tomb-raider-king",
+        title: "Tomb Raider King ราชันย์จอมโจรปล้นสุสาน",
+        image: "https://imgur.com/uM6epwk.png"
+      },
+      {
+        href: "https://www.reborntrans.com/martial-god-regressed-to-level-2",
+        title: "Martial God Regressed to Level 2",
+        image: "https://imgur.com/QbXiwQL.png"
+      },
+      {
+        href: "https://www.reborntrans.com/player-who-returned",
+        title: "Player Who Returned 10,000 Years Later เพลเยอร์ผู้กลับมาในรอบหมื่นปี",
+        image: "https://imgur.com/cjNFMyx.png"
+      },
+      {
+        href: "https://www.reborntrans.com/reincarnator",
+        title: "Reincarnator",
+        image: "https://imgur.com/ED9bJ5n.png"
+      },
+      {
+        href: "https://www.reborntrans.com/magic-academys-genius-blinker",
+        title: "Magic Academy Genius Blinker",
+        image: "https://imgur.com/qNEhI8o.png"
+      },
+      {
+        href: "https://www.reborntrans.com/reborn-as-the-enemy-prince",
+        title: "Reborn As The Enemy Prince เกิดใหม่เป็นเจ้าชายในประเทศศัตรู",
+        image: "https://imgur.com/BVlYlM6.png"
+      },
+      {
+        href: "https://www.reborntrans.com/i-kill-academy-player",
+        title: "I Killed an Academy Player เมื่อผมอยากฆ่าเพลเยอร์",
+        image: "https://imgur.com/goEk6FZ.png"
+      },
+      {
+        href: "https://www.reborntrans.com/dragon-devouring-mage",
+        title: "Dragon Devouring Mage จอมเวทผู้กลืนกินมังกร",
+        image: "https://imgur.com/OB2mqs0.png"
+      },
+      {
+        href: "https://toomtam-manga.com/manga/special-civil-servant",
+        title: "Special Civil Servant | กองกำลังพิเศษหมอกสีเหลือง",
+        image: "https://imgur.com/PHifIYB.png"
+      },
+      {
+        href: "https://toomtam-manga.com/manga/a-wizards-restaurant",
+        title: "A Wizard’s Restaurant",
+        image: "https://imgur.com/jVjK3eL.png"
+      },
+      {
+        href: "https://www.flash-manga.com/manga/genius-archers-streaming",
+        title: "Genius Archer’s Streaming",
+        image: "https://imgur.com/TE8vvZS.png"
+      },
+      {
+        href: "https://rose-manga.com/series/the-beginning-after-the-end",
+        title: "The Beginning After The End",
+        image: "https://imgur.com/EbyRyNq.png"
+      },
+      {
+        href: "https://popsmanga.com/manga/sss-class-suicide-hunter",
+        title: "SSS-Class Suicide Hunter",
+        image: "https://i.imgur.com/JL8tkoG.png"
+      },
+      {
+        href: "https://www.xn--l3c0azab5a2gta.com/manga/ranker-who-lives-a-second-time",
+        title: "Ranker Who Lives A Second Time",
+        image: "https://i.imgur.com/2KSWJTs.png"
+      },
+      {
+        href: "https://spy-manga.com/manga/the-part-time-land-of-the-gods",
+        title: "The Part-Time Land of the Gods",
+        image: "https://i.imgur.com/YlUEbMo.png"
+      },
+      {
+        href: "https://www.sixmanga.com/manga/the-constellations-are-my-disciples",
+        title: "The Constellations Are My Disciples",
+        image: "https://i.imgur.com/SNqqGNZ.png"
+      },
+      {
+        href: "https://www.nabee-manga.com/manga/transcension-academy",
+        title: "Transcension Academy",
+        image: "https://i.imgur.com/qlkGX2b.png"
+      },
+      {
+        href: "https://ped-manga.com/manga/omniscient-reader",
+        title: "Omniscient Reader Viewpoint",
+        image: "https://i.imgur.com/scJQK5T.png"
+      },
+      {
+        href: "https://ped-manga.com/manga/i-obtained-a-mythic-item",
+        title: "I Obtained a Mythic Item",
+        image: "https://i.imgur.com/1tsnjFi.png"
+      },
+      {
+        href: "https://www.sixmanga.com/manga/terminally-ill-genius-dark-knight",
+        title: "Terminally-Ill Genius Dark Knight",
+        image: "https://i.imgur.com/LA8Bczn.png"
+      },
+      {
+        href: "https://toomtam-manga.com/manga/the-world-after-the-fall",
+        title: "The World After The End",
+        image: "https://i.imgur.com/b1DScty.png"
+      },
+      {
+        href: "https://www.mangakimi.com/manga/trash-of-the-counts-family",
+        title: "Trash of the Count’s Family",
+        image: "https://i.imgur.com/WSWgitO.png"
+      },
+      {
+        href: "https://www.108-manga.com/manga/my-lucky-encounter-from-the-game-turned-into-reality",
+        title: "My Lucky Encounter From The Game Turned Into Reality",
+        image: "https://i.imgur.com/wmcVUv9.png"
+      },
+      {
+        href: "https://spy-manga.com/manga/foreigner-on-the-periphery",
+        title: "Foreigner on the Periphery",
+        image: "https://i.imgur.com/iDYkwlr.png"
+      },
+      {
+        href: "https://popsmanga.com/manga/the-youngest-son-of-a-master-swordsman",
+        title: "The Youngest Son of a Master Swordsman",
+        image: "https://i.imgur.com/aMmcfh0.png"
+      },
+      {
+        href: "https://manga-lc.net/manga/regressors-life-after-retirement",
+        title: "Regressor’s Life After Retirement",
+        image: "https://imgur.com/tMwiC6F.png"
+      },         
+      {
+        href: "https://www.sixmanga.com/manga/infinite-mage",
+        title: "Infinite Mage",
+        image: "https://imgur.com/OZOAqRw.png"
+      },
+      {
+        href: "https://www.sing-manga.com/manga/the-emperors-sword",
+        title: "The Emperor’s Sword",
+        image: "https://imgur.com/Vv9oECd.png"
+      },
+      {
+        href: "https://www.sing-manga.com/manga/my-exclusive-tower-guide",
+        title: "My Exclusive Tower Guide",
+        image: "https://imgur.com/WkFm5gS.png"
+      },
+      {
+        href: "https://www.mangakimi.com/manga/sss-class-gacha-hunter",
+        title: "SSS-Class Gacha Hunter ฮันเตอร์กาชาระดับ SSS",
+        image: "https://imgur.com/wzh1b5u.png"
+      },
+      {
+        href: "https://asurahunter.com/manga/the-descent-of-the-demonic-master",
+        title: "The Descent of the Demonic Master",
+        image: "https://imgur.com/5nu3Hj8.png"
+      },
+      {
+        href: "https://toomtam-manga.com/manga/return-of-the-frozen-player",
+        title: "Return Of The Frozen Player",
+        image: "https://imgur.com/eTAt75L.png"
+      },
+      {
+        href: "https://www.xn--72ca0fgy7cem.com/series/superstar-from-age-0",
+        title: "Superstar From Age 0",
+        image: "https://imgur.com/9jdZSpn.png"
+      },
+      {
+        href: "https://www.inu-manga.com/manga/hello-veterinarian",
+        title: "Hello! Veterinarian!",
+        image: "https://imgur.com/4zOXo8N.png"
+      },
+      {
+        href: "https://www.flash-manga.com/manga/be-the-actor",
+        title: "Be the Actor",
+        image: "https://i.imgur.com/wmKGtxg.jpg"
+      },
+      {
+        href: "https://www.snap-manga.com/manga/this-life-starts-as-a-child-actor",
+        title: "This Life Starts as a Child Actor",
+        image: "https://i.imgur.com/vUhuIAP.png"
+      },
+
+    ];
